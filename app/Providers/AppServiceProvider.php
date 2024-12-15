8f73f96bf473741a3ebe810ab41818806ba788da
aidil <aidil.ikbar@kpk.go.id>
2024-12-15 21:02:44 +0100

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,16 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+    }
     /**
      * Register any application services.
      */
